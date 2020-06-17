dabb72a898aef02624cff85750e0571ef705cfaf
destroyedlolo <l_faillie@yahoo.com>
2020-06-17 23:12:01 +0200

    V1.0

--- PubSysFigures.c
+++ PubSysFigures.c
@@ -4,7 +4,7 @@
  *
  * 	Compilation
  *
-gcc -lpthread -lpaho-mqtt3c -Wall PubSysFigures.c -o PubSysFigures
+gcc -lpaho-mqtt3c -Wall PubSysFigures.c -o PubSysFigures
  *
  * Copyright 2015 Laurent Faillie
  *
@@ -19,13 +19,13 @@ gcc -lpthread -lpaho-mqtt3c -Wall PubSysFigures.c -o PubSysFigures
  *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  *	16/06/2020 - v0.1 LF - Start of development
+ *	17/06/2020 - v1.0 LF - First workable version
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
-#include <pthread.h>
 #include <stdbool.h>
 #include <string.h>
 #include <libgen.h>		/* basename */
@@ -34,7 +34,7 @@ gcc -lpthread -lpaho-mqtt3c -Wall PubSysFigures.c -o PubSysFigures
 
 #include <MQTTClient.h>
 
-#define VERSION "0.1"
+#define VERSION "1.0"
 
 #define MAXLINE 1024	/* Maximum length of a line to be read */
 #define BRK_KEEPALIVE 60	/* Keep alive signal to the broker */
@@ -68,8 +68,37 @@ struct Config {
 	MQTTClient client;
 	const char *hostname;
 	size_t hnlen;
-	size_t topiclen;
 } cfg;
+
+	/*
+	 * MQTT's
+	 */
+
+int msgarrived(void *ctx, char *topic, int tlen, MQTTClient_message *msg){
+	if(cfg.verbose)
+		printf("*I* Unexpected message arrival (topic : '%s')\n", topic);
+
+	MQTTClient_freeMessage(&msg);
+	MQTTClient_free(topic);
+	return 1;
+}
+
+void connlost(void *ctx, char *cause){
+	printf("*W* Broker connection lost due to %s\n", cause);
+}
+
+int papub( const char *topic, int length, void *payload, int retained ){	/* Custom wrapper to publish */
+	MQTTClient_message pubmsg = MQTTClient_message_initializer;
+	pubmsg.retained = retained;
+	pubmsg.payloadlen = length;
+	pubmsg.payload = payload;
+
+	return MQTTClient_publishMessage( cfg.client, topic, &pubmsg, NULL);
+}
+
+	/*
+	 * Let's go
+	 */
 
 void theend(void){
 /* Some cleanup 
@@ -84,12 +113,14 @@ int main(int ac, char **av){
 	cfg.verbose = false;
 	cfg.id = NULL;
 	cfg.topic = NULL;
+	MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+	conn_opts.reliable = 0;
 
-	char l[MAXLINE];
+	char l[MAXLINE], *param;
 	FILE *f;
+	int i;
 
 	if(ac > 0){
-		int i;
 		for(i=1; i<ac; i++){
 			if(!strcmp(av[i], "-h")){
 				fprintf(stderr, "%s (%s)\n"
@@ -133,8 +164,10 @@ int main(int ac, char **av){
 
 	fclose(f);
 
-	if(cfg.verbose)
+	if(cfg.verbose){
+		printf("*I* Broker = \"%s\" port = %d\n", cfg.hostname, cfg.Broker_Port);
 		printf("*I* Hostname = \"%s\"\n", cfg.hostname);
+	}
 
 	if(!cfg.id){
 		assert( (cfg.id = malloc( 5 + cfg.hnlen)) );	/* 5: psf_ + \0 */
@@ -149,8 +182,70 @@ int main(int ac, char **av){
 		strcpy( cfg.topic, "Machines/" );
 		strcpy( cfg.topic+9, cfg.hostname );
 	}
-	cfg.topiclen = strlen(cfg.topic);
+//	cfg.topiclen = strlen(cfg.topic);
 	if(cfg.verbose)
 		printf("*I* Topics' root = \"%s\"\n", cfg.topic);
 
+	MQTTClient_create( &cfg.client, cfg.Broker_Host, "TeleInfod", MQTTCLIENT_PERSISTENCE_NONE, NULL);
+	MQTTClient_setCallbacks( cfg.client, NULL, connlost, msgarrived, NULL);
+
+	switch( MQTTClient_connect( cfg.client, &conn_opts) ){
+	case MQTTCLIENT_SUCCESS : 
+		break;
+	case 1 : fputs("Unable to connect : Unacceptable protocol version\n", stderr);
+		exit(EXIT_FAILURE);
+	case 2 : fputs("Unable to connect : Identifier rejected\n", stderr);
+		exit(EXIT_FAILURE);
+	case 3 : fputs("Unable to connect : Server unavailable\n", stderr);
+		exit(EXIT_FAILURE);
+	case 4 : fputs("Unable to connect : Bad user name or password\n", stderr);
+		exit(EXIT_FAILURE);
+	case 5 : fputs("Unable to connect : Not authorized\n", stderr);
+		exit(EXIT_FAILURE);
+	default :
+		fputs("Unable to connect : Unknown version\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+	atexit(theend);
+
+	/*
+	 * Publish statics'
+	 */
+	if(!(f=fopen("/sys/devices/system/cpu/kernel_max", "r"))){
+		perror("/sys/devices/system/cpu/kernel_max");
+		exit(EXIT_FAILURE);
+	}
+	fscanf(f, "%d", &i);
+	i++;
+	fclose(f);
+	if(cfg.verbose)
+		printf("*I* cpus = \"%d\"\n", i);
+	sprintf( l, "%s/ncpu", cfg.topic);
+	param = l + strlen(l) + 2;
+	sprintf( param, "%d", i);
+	papub( l, strlen(param), param, true );
+
+	if(!(f=fopen("/proc/loadavg", "r"))){
+		perror("/proc/loadavg");
+		exit(EXIT_FAILURE);
+	}
+	float r1,r5,r10;
+	fscanf(f, "%f %f %f", &r1, &r5, &r10);
+	fclose(f);
+	if(cfg.verbose)
+		printf("*I* loadav = %.2f %.2f %.2f\n", r1, r5, r10);
+	sprintf( l, "%s/Load/1", cfg.topic);
+	param = l + strlen(l) + 2;
+	sprintf( param, "%.2f", r1);
+	papub( l, strlen(param), param, true );
+	sprintf( l, "%s/Load/5", cfg.topic);
+	param = l + strlen(l) + 2;
+	sprintf( param, "%.2f", r5);
+	papub( l, strlen(param), param, true );
+	sprintf( l, "%s/Load/10", cfg.topic);
+	param = l + strlen(l) + 2;
+	sprintf( param, "%.2f", r10);
+	papub( l, strlen(param), param, true );
+	
+	exit(EXIT_SUCCESS);
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # PubSysFigures
 Publish unix system figure as MQTT topics
+
+# retained published topics
+
+These topics are published once at daemon starting with the hardware configuration of the host.
+
+* **.../ncpu** : number of cores
+
+# published topics
+
+* **.../Load/1** : loadaverage at 1 minutes
+* **.../Load/5** : loadaverage at 5 minutes
+* **.../Load/10** : loadaverage at 10 minutes
